896d04d8ea51186a57768cd47cbd109f8b253d24
Fu Diwei <fudiwei@sina.com>
2021-02-05 00:19:32 +0800

    feat: rewrite TS declaration

--- com-loading.d.ts
+++ com-loading.d.ts
@@ -1,18 +1,18 @@
-export interface ComponentLoading {
+export interface WepyComponentLoading {
     /**
      * 显示加载对话框。
      * @param {String | Object} options
      */
-    show(
-        options?:
-            | string
-            | {
-                  content: string;
-              }
-    ): void;
+    show(options?: string | WepyComponentLoading.LoadingOptions): void;
 
     /**
      * 隐藏加载对话框。
      */
     hide(): void;
+}
+
+export namespace WepyComponentLoading {
+    export interface LoadingOptions {
+        content: string;
+    }
 }
